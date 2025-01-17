67dc3a7793227887b8ca31c18f5be3f550f592eb
dagsdags212 <jegsamson.dev@gmail.com>
2025-01-17 08:21:24 +0800

    updated install.sh: source aliases.sh from the RC file

--- install.sh
+++ install.sh
@@ -2,14 +2,17 @@
 
 PROJECT_ROOT=$(dirname $(realpath "$0"))
 
-# ln -s -f ${PROJECT_ROOT}/src/fetch.mk fetch
-
+# Path where bioflow will live in the local system
 LOCAL_DIR=~/.local/share/bioflow
 
+RC_FILE=~/.zshrc
+
+# Create a symbolic link to each makefile workflow
 symlink_src() {
-  mkdir -p ${LOCAL_DIR}/bin
+  local target=${LOCAL_DIR}/bin
+  mkdir -p ${target}
   for mk in $(find ${PROJECT_ROOT}/src -name "*.mk"); do
-    ln -s -f ${mk} ${LOCAL_DIR}/bin
+    ln -s -f ${mk} ${target}
   done
 }
 
@@ -17,6 +20,7 @@ create_aliases() {
   local target=${LOCAL_DIR}/aliases.sh
   rm ${target}
   touch ${target}
+  # Treat each makefile as a binary executable
   echo "alias bf-align=\"make -f ${LOCAL_DIR}/bin/alignment.mk\"" >${target}
   echo "alias bf-phylo=\"make -f ${LOCAL_DIR}/bin/phylo.mk\"" >>${target}
   echo "alias bf-vc=\"make -f ${LOCAL_DIR}/bin/variant_calling.mk\"" >>${target}
@@ -25,12 +29,17 @@ create_aliases() {
   echo "alias bf-assemble=\"make -f ${LOCAL_DIR}/bin/assembly.mk\"" >>${target}
   echo "alias bf-fetch=\"make -f ${LOCAL_DIR}/bin/fetch.mk\"" >>${target}
   echo "alias bf-annotate=\"make -f ${LOCAL_DIR}/bin/annotation.mk\"" >>${target}
+}
 
+append_to_rc() {
+  grep -qxF "source \"/home/dagsdags/.local/share/bioflow/aliases.sh\"" ~/.zshrc || echo "source \"${LOCAL_DIR}/aliases.sh\"" >>${RC_FILE}
 }
 
 run_setup() {
   symlink_src
   create_aliases
+  append_to_rc
 }
 
+# Run complete setup
 run_setup
